3535ae54418c6a63fce5560d378017b63f277992
Endework <endeworkabera@gmail.com>
2024-05-11 03:21:36 +0300

    task 4 completed

--- scripts/EDA.py
+++
@@ -1,9 +0,0 @@
-"""
-Task1.1
-Aggregate per user the following information in the column  
-number of xDR sessions
-Session duration
-the total download (DL) and upload (UL) data
-the total data volume (in Bytes) during this session for each application
-
-"""
